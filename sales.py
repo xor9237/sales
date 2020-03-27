d62982130de2a65a6d30cc13c51c92022028abd2
xor9237 <xor9237@gmail.com>
2020-03-26 19:36:14 -0700

    second commit

---
+++ sales.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+## merge 12 months of sales data into a single file
+
+import os
+import glob
+
+# selecting working folder
+path="/Users/kitaeklee/Desktop/Sales Data/SalesAnalysis/Sales_Data"
+
+# collect all files needed to be combined
+all_files = glob.glob(os.path.join(path, "Sales_*.csv"))
+
+# read, concatenate
+merged_file = pd.concat((pd.read_csv(f, sep=',') for f in all_files), ignore_index=True)
+
+# export as CSV to the local directory
+merged_file.to_csv("/Users/kitaeklee/Desktop/Sales Data/SalesAnalysis/Sales_Data/merged_file.csv", index=False)
+print(merged_file.head())
+
+
