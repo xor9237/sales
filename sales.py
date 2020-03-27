7732335f5607a63d3c8e3d6e14ec3399606f4c76
xor9237 <xor9237@gmail.com>
2020-03-26 20:01:19 -0700

    second commit with data

--- sales.py
+++ sales.py
@@ -15,7 +15,7 @@ all_files = glob.glob(os.path.join(path, "Sales_*.csv"))
 merged_file = pd.concat((pd.read_csv(f, sep=',') for f in all_files), ignore_index=True)
 
 # export as CSV to the local directory
-merged_file.to_csv("/Users/kitaeklee/PycharmProjects/sales/Sales_Data/merged_file.csv", index=False)
+merged_file.to_csv("/Users/kitaeklee/PycharmProjects/sales/merged_file.csv", index=False)
 print(merged_file.head())
 
 
