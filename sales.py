47da607f2115441452492ae8013d19004fd07993
xor9237 <xor9237@gmail.com>
2020-03-26 19:53:09 -0700

    second commit with data

--- sales.py
+++ sales.py
@@ -6,7 +6,7 @@ import os
 import glob
 
 # selecting working folder
-path="/Users/kitaeklee/Desktop/Sales Data/SalesAnalysis/Sales_Data"
+path="/Users/kitaeklee/PycharmProjects/sales/Sales_Data"
 
 # collect all files needed to be combined
 all_files = glob.glob(os.path.join(path, "Sales_*.csv"))
@@ -15,7 +15,7 @@ all_files = glob.glob(os.path.join(path, "Sales_*.csv"))
 merged_file = pd.concat((pd.read_csv(f, sep=',') for f in all_files), ignore_index=True)
 
 # export as CSV to the local directory
-merged_file.to_csv("/Users/kitaeklee/Desktop/Sales Data/SalesAnalysis/Sales_Data/merged_file.csv", index=False)
+merged_file.to_csv("/Users/kitaeklee/PycharmProjects/sales/Sales_Data/merged_file.csv", index=False)
 print(merged_file.head())
 
 
