9bf468307527b0b0cd7fdab335d2579eee0fe0d1
xor9237 <xor9237@gmail.com>
2020-04-01 02:02:57 -0700

    new commit

--- sales.py
+++ sales.py
@@ -17,45 +17,60 @@ merged_file = pd.concat((pd.read_csv(f, sep=',') for f in all_files), ignore_ind
 # export as CSV to the local directory
 merged_file.to_csv("/Users/kitaeklee/PycharmProjects/sales/merged_file.csv", index=False)
 
-#add an additional column for the date
-merged_file2 = merged_file.assign(merged_file.at['Order Date'])
 
 
-# # The best month for sales and how much earned in that month
-# find and drop the rows with missing values or categorical values
-f = 0
+# # # 1. The best month for sales and how much earned in that month
 
-value = merged_file.at[f, 'Order ID']
-for value in merged_file:
-    if value is None:
-        merged_file.drop(merged_file.index[f])
-        merged_file.reset_index(inplace=True, drop=True)
-    elif value == 'Order ID':
-        merged_file.drop(merged_file.index[f])
-        merged_file.reset_index(drop=True, inplace=True)
-    elif f==186845:
-        break
-    else:
-        f+=1
+# Data Cleaning: Drop the rows with missing values or categorical values
 
-while f<186840:
-    if f<186840:
-        merged_file.dropna()
-        merged_file.select_dtypes(exclude=['object'])
-        f+=1
-    else:
-        break
+merged_file.dropna(subset=['Order ID'],inplace=True)
+import numpy as np
+merged_file = merged_file[merged_file['Order ID'].apply(lambda x: np.isreal(x))]
+merged_file.reset_index(inplace=True, drop=True)
 
 
+#f = 0
+
+#value = merged_file.at[f, 'Order ID']
+#for value in merged_file:
+#    if value is None:
+#        merged_file.drop(merged_file.index[f])
+#        merged_file.reset_index(inplace=True, drop=True)
+#        f+=1
+#        continue
+#    elif value == 'Order ID':
+#        merged_file.drop(merged_file.index[f])
+#        merged_file.reset_index(drop=True, inplace=True)
+#        f+=1
+#        continue
+#    elif f==186845:
+#        break
+#    else:
+#        f+=1
+#        continue
+
+#while f<186840:
+#    if f<186840:
+#        merged_file.dropna()
+#        f+=1
+#    else:
+#        break
 
 
 
 
-
-print(merged_file.head())
+#print(merged_file.head())
 print(merged_file.at[253, 'Order ID'])
 print(merged_file.at[254, 'Order ID'])
 print(merged_file.at[255, 'Order ID'])
+
+# add an additional column for the month of order date
+#merged_file['Order Month'] = merged_file['Order Date'].str[:2]
+#merged_file['Order Month'] = merged_file['Order Month'].astype('int32', copy=False)
+
+
+
+
 
 
 # get the revenue
